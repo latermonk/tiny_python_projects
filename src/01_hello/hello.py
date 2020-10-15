1b6c547225b3f4227196c0b53772a2e3be047935
latermonk <sprout_wang@163.com>
2020-10-16 05:46:48 +0800

    cc

--- src/01_hello/hello.py
+++ src/01_hello/hello.py
@@ -1,3 +1,25 @@
 #! /usr/bin/env python3
 
-print('Hello, World!')
+
+import argparse
+import pathlib
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('name', help='Name to greet')
+    #
+    #
+    args = parser.parse_args()
+    #
+    # print(pathlib.Path(__file__).parent.absolute())
+    #
+    print('Hello, World!   ' + args.name)
+
+
+
+def test():
+    print(pathlib.Path(__file__).parent.absolute())
+
+if __name__ == '__main__':
+    test()
--- src/01_hello/hello04_argparse_positional.py
+++ src/01_hello/hello04_argparse_positional.py
@@ -6,4 +6,8 @@ import argparse
 parser = argparse.ArgumentParser(description='Say hello')
 parser.add_argument('name', help='Name to greet')
 args = parser.parse_args()
+
+
 print('Hello, ' + args.name + '!')
+
+
